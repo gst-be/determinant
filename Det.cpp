8bd25ed68a08b47cea6af3816e103c7ebb535e81
gst-be <113920720+gst-be@users.noreply.github.com>
2022-09-30 22:43:09 +0800

    Update Det.cpp

--- Det.cpp
+++ Det.cpp
@@ -1,54 +1,59 @@
 #include <iostream>
 #include <stdio.h>
-#define N 999
+#define N 900//比n^n大即可
+#define max_n 30//最高可算多少阶行列式
 
 using namespace std;
 
+//计算每项前系数
 int t(int p[N],int n)
 {	
-	int c=0;
+	int c=0;//定义逆序数计数器
 	
 	for(int i=1;i<=n;i++)
 	{
 		for(int j=i+1;j<=n;j++)
 		{
-			if(p[i]>p[j])
-			c++;
+			if(p[i]>p[j])  
+			c++;//出现逆序则自增1
 			else if(p[i]==p[j])
-			return 0;
+			return 0;//出现同列情况将系数变为0
 		}
 	}
 	
 	if(c%2==0)
-	return 1;
+	return 1;//偶逆序数返回1
 	else
-	return -1;
+	return -1;//奇逆序数返回-1
 }
 
-double product(double x[30][30],int p[N],int n)
+//计算系数外每项的乘积
+double product(double x[max_n][max_n],int p[N],int n)
 {
-	double pdt=1;
+	double pdt=1;//乘积初始化为1
 	for(int i=1;i<=n;i++)
 	{
 		pdt*=x[i][p[i]];
 	}
-	return pdt;
+	return pdt;//返回本次序列数所对应的乘积
 }
 
 
-double sum(double x[30][30],int n)
+double sum(double x[max_n][max_n],int n)
 {
-	double sum1=0;
-	int p[N];
+	double sum1=0;//初始化结果为0
+	int p[N];//定义序列数，从p[1]开始每一位的值对应取x[][]中每行的第几列
 	for(int i=1;i<=N;i++)
 	{
-		p[i]=1;
+		p[i]=1;//初始化
 	}
+	
 	
 	for(int i=1;i<=n^n;i++)
 	{
+		//开始生成序列数
 		p[1]++;
-		for(int j=1;j<=n^n;j++)
+		for(int j=1;j<=n;j++)
 		{
 			if(p[j]==n+1)
 			{
@@ -67,9 +72,11 @@ double sum(double x[30][30],int n)
 int main()
 {
 	int n;
-	double x[30][30];
+	double x[max_n][max_n];
 	cout<<"input n=";
-	cin>>n;
+	cin>>n;//定义行列式阶数
+
+	//输入行列式的每一项的值
 	for(int i=1;i<=n;i++)
 	{
 		for(int j=1;j<=n;j++)
